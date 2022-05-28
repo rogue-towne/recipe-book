a3132087101f06729add37439f1f122b0c568378
rogue-towne <69937144+rogue-towne@users.noreply.github.com>
2022-05-27 20:24:51 -0600

    Implemented delete/put user routes

--- routes/user.js
+++ routes/user.js
@@ -16,7 +16,7 @@ router.post('/', async (req, res) => {
         res.status(400).json({message: err.message})
     }
   })
-
+// Retrieve all users
 router.get('/', async (req, res) => {
 try {
     const user = await User.find()
@@ -25,5 +25,44 @@ try {
     res.status(500).json({ message: err.message})
 }
 })
+// Modify a user
+router.put('/:id', getUser, async (req, res) => {
+    if (req.body.username != null){
+        res.user.username = req.body.username
+    }
+    if (req.body.password != null){
+      res.user.password = req.body.password
+    }
+    try {
+        const updatedUser = await res.user.save()
+        res.json(updatedUser)
+    } catch (err){
+        res.status(400).json({message: err.message})
+    }
+  })
+  
+//Delete A User
+router.delete('/:id', getUser, async (req, res) => {
+try {
+    await res.user.remove()
+    res.json({message: "Deleted User"})
+} catch (err) {
+    res.status(500).json({message: err.message})
+}
+})
+  
+async function getUser(req, res, next) {
+let user;
+try {
+    user = await User.findById(req.params.id)
+    if (user == null) {
+        return res.status(404).json({message: 'Cannot find User'})
+    }
+} catch (err){
+    return res.status(500).json({message: err.message})
+}
+res.user = user
+next()
+}
 
 module.exports = router;
