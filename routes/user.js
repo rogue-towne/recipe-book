b5c5bb422ebb434a7fef5cac04b33162d4b13939
rogue-towne <69937144+rogue-towne@users.noreply.github.com>
2022-06-18 12:19:04 -0600

    fixed swagger urls

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
-const { requiresAuth } = require('express-openid-connect');
 
 // //Create a User
 // router.post('/', async (req, res) => {
