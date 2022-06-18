471adfc611ea56b14148e8c078549d49fed4010e
rogue-towne <69937144+rogue-towne@users.noreply.github.com>
2022-06-18 09:53:10 -0600

    add recipe route to index

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 router.use('/', require('./home'));
 router.use('/user', require('./user'));
+router.use('/recipe', require('./recipe'));
 router.use('/', require('./swagger'));
 
 module.exports = router;
